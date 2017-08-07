e62a787a03d293be919458efabdb24841dfd9713
Sandra Schuhmacher <coding@voiding-warranties.de>
2017-08-07 20:50:15 +0200

    (fix) found the bug in object_recognition and fixed it, project works now Resolves: #13 Resolves: #1

--- Exercise-3/sensor_stick/scripts/object_recognition.py
+++ Exercise-3/sensor_stick/scripts/object_recognition.py
@@ -1,25 +1,12 @@
 #!/usr/bin/env python
 
 import numpy as np
-import sklearn
-from sklearn.preprocessing import LabelEncoder
 from segmentation import *
 
 import pickle
 
-from sensor_stick.srv import GetNormals
-from sensor_stick.features import compute_color_histograms
-from sensor_stick.features import compute_normal_histograms
 from visualization_msgs.msg import Marker
-
-from sensor_stick.marker_tools import *
-from sensor_stick.msg import DetectedObjectsArray
-from sensor_stick.msg import DetectedObject
 from sensor_stick.pcl_helper import *
-
-def get_normals(cloud):
-    get_normals_prox = rospy.ServiceProxy('/feature_extractor/get_normals', GetNormals)
-    return get_normals_prox(cloud).cluster
 
 # Callback function for your Point Cloud Subscriber
 def pcl_callback(pcl_msg):
@@ -41,8 +28,10 @@ def pcl_callback(pcl_msg):
     
 
     # TODO: Euclidean Clustering
+    cluster_indices, white_cloud = euclid_cluster( cloud_objects )
+
     # TODO: Create Cluster-Mask Point Cloud to visualize each cluster separately
-    cluster_cloud = euclid_cluster( cloud_objects )
+    cluster_cloud = cluster_mask( cluster_indices, white_cloud )
 
     # TODO: Convert PCL data to ROS messages
     ros_cloud_objects =  pcl_to_ros( cloud_objects )
@@ -51,40 +40,8 @@ def pcl_callback(pcl_msg):
 
     # Exercise-3 TODOs: 
     # Classify the clusters! (loop through each detected cluster one at a time)
-    detected_objects_labels = []
-    detected_objects = []
+    detected_objects, detected_objects_labels = classify_cluster(cluster_indices, white_cloud, cloud_objects, clf, encoder, scaler, object_markers_pub)
 
-        # Grab the points for the cluster
-    for index, pts_list in enumerate(cluster_indices):
-        # Grab the points for the cluster
-        pcl_cluster = cloud_out.extract(pts_list)
-        # TODO: convert the cluster from pcl to ROS using helper function
-	ros_cluster = pcl_to_ros(pcl_cluster)
-
-        # Extract histogram features
-        # TODO: complete this step just as you did before in capture_features.py
-	chists = compute_color_histograms(sample_cloud, using_hsv=True)
-        normals = get_normals(ros_cloud)
-        nhists = compute_normal_histograms(normals)
-        feature = np.concatenate((chists, nhists))
-        labeled_features.append([feature, model_name])
-
-        # Make the prediction, retrieve the label for the result
-        # and add it to detected_objects_labels list
-        prediction = clf.predict(scaler.transform(feature.reshape(1,-1)))
-        label = encoder.inverse_transform(prediction)[0]
-        detected_objects_labels.append(label)
-
-        # Publish a label into RViz
-        label_pos = list(white_cloud[pts_list[0]])
-        label_pos[2] += .4
-        object_markers_pub.publish(make_label(label,label_pos, index))
-
-        # Add the detected object to the list of detected objects.
-        do = DetectedObject()
-        do.label = label
-        do.cloud = ros_cluster
-        detected_objects.append(do)
 
     rospy.loginfo('Detected {} objects: {}'.format(len(detected_objects_labels), detected_objects_labels))
 
